cc14fed2b899ab2d8ef331353f2d4bcf3b6f9565
Clementine <clementine.mory@gmail.com>
2023-09-29 21:03:12 +0200

    backend morning news

--- README.md
+++
@@ -1,1 +0,0 @@
-# Morningnews-Backend
--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = 'mongodb+srv://clementinemory:eapBhjlvmRYgiv5O@cluster0.notfyka.mongodb.net/morning-news';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
